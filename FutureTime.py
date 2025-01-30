12788f1546aecca7c0b73b1c4be0e9befd4825f3
Jschnakenberg06 <jonasschnakenberg@unomaha.edu>
2025-01-30 22:26:15 +0000

    Done with Lab 2

--- FutureTime.py
+++ FutureTime.py
@@ -1,7 +1,7 @@
 #FutureTime.py
-#Name:
-#Date:
-#Assignment:
+#Name:Jonas Schnakeneberg
+#Date:1/30/2025
+#Assignment:Future Time
 
 # datetime will allow us to access the system date and time.
 import datetime
@@ -13,16 +13,36 @@ def main():
   currentMinute = now.minute
 
   print (currentHour, currentMinute) #this is just for checking, we should delete it later
+  print("Future Time Calculator")
 
   #TODO:
   #Ask user for hours
   #Ask user for minutes
+  
 
   #Calculate the time after the user-supplied time has passed.
+  # Getting curent time from system, storing to variable
+  now = datetime.datetime.now()
+  currentMinute = (now.minute)
+  currentHour = (now.hour - 6) % 24
+
 
   #Do not use any if statements in calculating the time.
+  # Ask user for input
+  moreMins = int(input("Enter minutes to add: "))
+  moreHours= int(input("Enter hours to add: "))
 
   #Output the future time in standard format "HH:MM"
+  # Calculate the New Time
+  extraHours = (currentMinute + moreMins) // 60
+  futureHours = (currentHour + moreHours + extraHours) % 24
+  futureMinutes = (currentMinute + moreMins) % 60
+  
+
+  print("Future Time: " + str(futureHours) + ":" + str(futureMinutes))
+  print(futureMinutes)
+  print(futureHours)
+
 
 
 if __name__ == '__main__':
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,7 +1,7 @@
 #Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
+#Name:Jonas Schnakenberg 
+#Date:1/30/2025
+#Assignment:Magic 8 Ball
 
 #We will need random for this program, import to use this package.
 import random
@@ -10,9 +10,17 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
-
+  question = input("What do you wish for?")
+  print (question)
   #Answer question randomly with one of the options from your earlier list.
-
-
+  answers = ("As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.",
+  "Don’t count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.", 
+  "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Yes.", "Yes – definitely.", "You may rely on it.")
+  length=len(answers)
+  r=random.random() * length
+  r=int(r)
+  print(r)
+  response=answers[r]
+  print(response)
 if __name__ == '__main__':
   main()
